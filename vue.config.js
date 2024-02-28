21f339745a7f47944723ff27f634d1d2df2a5d2b
Mike <prince-poley@yandex.ru>
2024-02-28 22:10:12 +0300

    Modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-vuex-vue-router-todo-app/'
-    : '/'
+    : '/',
+  indexPath: "./dist/index.html",
 });
