32cf0ba9dea125a1f8ca5ea22d85333a1ff717be
Mike <prince-poley@yandex.ru>
2024-02-28 22:14:45 +0300

    Revert

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,4 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-vuex-vue-router-todo-app/'
-    : '/',
-  indexPath: "./dist/index.html",
 });
